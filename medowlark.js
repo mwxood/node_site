26a5466aeba612f50c89e0e49dba44d217b80689
Mihail <mwxood@users.noreply.github.com>
2021-10-26 18:51:58 +0300

    first commit

---
+++ medowlark.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+const app = express()
+const port = process.env.PORT || 3000
+
+
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main'
+}))
+
+
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => res.render('home'))
+app.get('/about', (req, res) => res.render('about'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.use((req, res) => {
+    res.status(404)
+    res.render('404')
+})
+
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}` + ' Click CTRL+C to close app'
+));
